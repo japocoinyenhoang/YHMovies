d54de13c0d30f8fdf30fb55800ba1b54ccb3c176
Yen Hoang <japocoin@gmail.com>
2021-04-24 06:44:10 +0200

    moviesAdapter added

--- app/src/main/java/com/yenhoang/yhmovies/MainActivity.kt
+++ app/src/main/java/com/yenhoang/yhmovies/MainActivity.kt
@@ -19,12 +19,26 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)  //generamos un objeto que puede acceder a todas las vistas
         setContentView(binding.root)  // el root es la raiz del componente que hemos inflado.
 
-        binding.message.text = "Hello Androoid"
+        //binding.message.text = "Hello Androoid"
 
         /* // con el apply
          super.onCreate(savedInstanceState)
          ActivityMainBinding.inflate(layoutInflater).apply {
              setContentView(root)
              message.text = "Hello Android"*/
+
+        //el adapter es el qie identifica como se va a pintar dentro del reciclerView
+        //tenemos que crear una clase que en este caso llamaremos MoviesAdapter
+        binding.recycler.adapter = MoviesAdapter(
+                listOf(
+                        Movie("Title 1", "url 1"),
+                        Movie("Title 2", "url 2"),
+                        Movie("Title 3", "url 3"),
+                        Movie("Title 4", "url 4"),
+                        Movie("Title 6", "url 6"),
+                        Movie("Title 7", "url 7"),
+                        Movie("Title 8", "url 8")
+                )
+        )
     }
 }
---
+++ app/src/main/java/com/yenhoang/yhmovies/MoviesAdapter.kt
@@ -0,0 +1,52 @@
+package com.yenhoang.yhmovies
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yenhoang.yhmovies.databinding.ViewMovieItemBinding
+
+
+// teien que extender de una clase especifica que se llama RecicletView.Adapter y a su vez necesita un viewHolder ya que nos pide el tipo en <>
+// despues de crear la clase Movie le decimos que va a recibir  "(private val movies: List<Movie>)"
+// tb hay que crear un nuevo layout que represente cada elemento de la lista  yque va a inflar la vista el moviesAdapter
+class MoviesAdapter(private val movies: List<Movie>): RecyclerView.Adapter<MoviesAdapter.ViewHolder> () {
+
+    // x otro lado el ViewHolder recibe una vista "(view: View)"
+    //el ViewHolder es el componente dentro del adapter que va a contener la vista
+    //class ViewHolder( view: View) : RecyclerView.ViewHolder(view)
+    class ViewHolder( private val binding: ViewMovieItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie){
+            binding.movieTitle.text = movie.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //este crea una nueva vista cuadno el reciclerView se lo pida
+        // val view = LayoutInflater.from(parent.context).inflate(R.layout.view_movie_item, parent, false)
+        //return ViewHolder(view)
+
+
+        //con viewBinding
+        val binding = ViewMovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // este va a actualizar una vista cuando el adapter se lo pida
+        holder.bind(movies[position])
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+        // en funcion de la posicion que viewType necesitamos
+    }
+
+    override fun getItemCount(): Int {
+        // este va a devolver el numero de elementos del adapter
+        return movies.size
+    }
+}
+
+
+// Para traernos la informacion, previamente tenemos que crearnos una clase, en nuestro caso, la clase Movie
